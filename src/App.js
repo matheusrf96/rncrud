268240e971c1fbc11accb996475edfe72b8bdf95
Matheus Rodrigues <matheusrf96@gmail.com>
2021-02-23 22:12:23 -0300

    Implement basic navigation, pt. II and add Font Awesome

--- src/App.js
+++ src/App.js
@@ -4,21 +4,48 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import UserList from './views/UserList'
 import UserForm from './views/UserForm'
+import { Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const Stack = createStackNavigator()
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: "#F4511E",
+    },
+    headerTintColor: "#FFF",
+    headerTitleStyle: "bold",
+}
 
 export default props => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="UserList">
+            <Stack.Navigator initialRouteName="UserList" screenOptions={ screenOptions }>
                 <Stack.Screen
                     name="UserList"
                     component={ UserList }
+                    options={({ navigation }) => {
+                        return {
+                            title: "Lista de Usuários",
+                            headerRight: () => {
+                                return (
+                                    <Button
+                                        onPress={() => navigation.navigate('UserForm')}
+                                        type="clear"
+                                        icon={<Icon name="plus" size={22} color="#fff" />}
+                                    />
+                                )
+                            }
+                        }
+                    }}
                 />
 
                 <Stack.Screen
                     name="UserForm"
                     component={ UserForm }
+                    options={{
+                        title: "Formulário de Usuários"
+                    }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
