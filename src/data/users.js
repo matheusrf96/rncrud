fe31f206158f60592e45e44757f256feff04af8c
Matheus Rodrigues <matheusrf96@gmail.com>
2021-03-08 21:35:31 -0300

    Create UserList page

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        name: 'Zata',
+        email: 'zata@megamail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/12/11/avatar-1300582_960_720.png',
+    },
+    {
+        id: 2,
+        name: 'Samira',
+        email: 'samira@megamail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png',
+    },
+    {
+        id: 3,
+        name: 'Paul',
+        email: 'paulsmith@hammer.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png',
+    },
+    {
+        id: 4,
+        name: 'Mr. Denvis',
+        email: 'mrdenvis@vipmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/05/26/14/20/hat-1417247_960_720.jpg',
+    },
+    {
+        id: 5,
+        name: 'Misha',
+        email: 'misha@hammer.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png',
+    },
+    {
+        id: 6,
+        name: 'Doggo',
+        email: 'doggo@megamail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png',
+    },
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,34 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList } from 'react-native'
+import { View } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+
+import users from '../data/users'
 
 export default props => {
+    const getUserItem = ({item: user}) => {
+        return (
+            <ListItem
+                key={user.id}
+                onPress={() => props.navigation.navigate('UserForm')}
+                bottomDivider
+            >
+                <Avatar source={{uri: user.avatarUrl}} rounded large />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+
     return (
-        <Text>User List</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            ></FlatList>
+        </View>
     )
 }
